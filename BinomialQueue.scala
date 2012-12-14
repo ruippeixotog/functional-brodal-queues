f62931440f9993ba6756d010783421724433293f
Rui Gonçalves <ruippeixotog@gmail.com>
2012-12-14 01:32:37 +0000

    Added proper test cases for BinomialQueue.

--- BinomialQueue.scala
+++ BinomialQueue.scala
@@ -12,6 +12,7 @@ object BinomialQueue {
     seq.foldLeft(List[Node[T]]())(_.insert(_))
 
   final class BinomialQueueOps[T <% Ordered[T]](self: BinomialQueue[T]) {
+
     def insert(e: T): BinomialQueue[T] = insert(Node(e, 0, Nil))
 
     def insert(n: Node[T]): BinomialQueue[T] = self match {
@@ -61,29 +62,4 @@ case class Node[T <% Ordered[T]](root: T, rank: Int, children: List[Node[T]]) {
     if (root < that.root) Node(root, rank + 1, that :: children)
     else Node(that.root, that.rank + 1, this :: that.children)
   }
-}
-
-object Main extends App {
-
-  import BinomialQueue._
-
-  val elems1 = Seq(3, 56, 8, 4, 6, 5)
-  val elems2 = Seq(-4, 6, 2, 10, 20, 30, 40)
-
-  val queue = BinomialQueue(elems1: _*)
-  val queue2 = BinomialQueue(elems2: _*)
-  val meldQueue = queue meld queue2
-
-  def extractMins[T <% Ordered[T]](q: BinomialQueue[T], nMins: Int): Seq[T] = {
-    (1 to nMins).foldLeft((Seq[T](), q)) {
-      case ((mins, qi), _) =>
-        (mins :+ qi.findMin, qi.withoutMin)
-    }._1
-  }
-
-  assert(extractMins(queue, elems1.length) == elems1.sorted, "elems1")
-  assert(extractMins(queue2, elems2.length) == elems2.sorted, "elems2")
-  assert(
-    extractMins(meldQueue, elems1.length + elems2.length)
-      == (elems1 ++ elems2).sorted, "meld")
 }
--- build.sbt
+++ build.sbt
@@ -1,3 +1,5 @@
 seq(Revolver.settings: _*)
 
 javaOptions in Revolver.reStart += "-Xmx4g"
+
+libraryDependencies += "org.specs2" %% "specs2" % "1.12.1" % "test"
